ec33ec30eeea0215d56ecddc6f7ae1367072ba62
EliasLuiz <eliasluizjr@gmail.com>
2014-07-22 10:38:05 -0300

    TipoExame alterado pra incluir MySQL.

--- classes/M/TipoExame.php
+++ classes/M/TipoExame.php
@@ -6,7 +6,11 @@
  * @author Daniel
  */
 
+include_once 'MySQL.php';
+
 class TipoExame {
+    
+    use MySQL;
     
     protected $nome;
     protected $coletadomicilio;
@@ -55,50 +59,52 @@ class TipoExame {
     }
     
     //Métodos de Banco de Dados
-    public function carregaMySQL($cdExame){
-        
-        //Estabelece conexão
-        $con = mysql_connect("localhost","root","") or die('Não foi possível estabelecer conexão com o banco de dados: '.mysql_error());
-        mysql_select_db('mydb') or die('Não foi possível selecionar o banco' . mysql_error());
+    public function carrega($cdExame){
         
         //Gera SQL e busca Exame no banco, carregando se não houver erro
         $sql = "SELECT * FROM TB_TipoExame t WHERE t.cdTipoExame = '" . $cdExame . "'";
         $result = mysql_query($sql, $con) or die('Não foi possível carregar TipoExame do banco de dados: '.mysql_error());
-            $result = mysql_fetch_array($result);
+        $result = mysql_fetch_array($result);
             
-            $this->nome = $result['nmTipoExame'];
-            $this->coletadomicilio = $result['coletaDomicilio'];
-            $this->requisitos = $result['requisitos'];
-            $this->info = $result['informacoes'];
-            $this->preco = $result['preco'];
-        
-        mysql_close($con);
+        $this->nome = $result['nmTipoExame'];
+        $this->coletadomicilio = $result['coletaDomicilio'];
+        $this->requisitos = $result['requisitos'];
+        $this->info = $result['informacoes'];
+        $this->preco = $result['preco'];
     }
-    public function salvaMySQL(){
-        //Estabelece conexão
-        $con = mysql_connect("localhost","root","") or die('Não foi possível estabelecer conexão com o banco de dados: '.mysql_error());
-        mysql_select_db('mydb') or die('Não foi possível selecionar o banco' . mysql_error());
+    public function salva(){
         
         //Gera SQL para salvar/atualizar Exame no banco
         $sql = "SELECT * FROM TB_TipoExame t WHERE t.nmTipoExame = '" . $this->nome . "'";
-        $result = mysql_query($sql) or die('Não foi possível buscar TipoExame no banco de dados: '.mysql_error());
-        $result = mysql_fetch_array($result);
+        $result = $this->query($sql) or die('Não foi possível buscar TipoExame'
+                . ' no banco de dados: '.mysql_error());
+        $result = $this->fetch_array($result);
+        
         if($result["nmTipoExame"]==$this->nome){
-            $sql = "UPDATE TB_TipoExame t SET t.nmTipoExame = '" . $this->nome .
-                   "', t.coletaDomicilio = '" . $this->coletadomicilio . "', t.requisitos = '" . $this->requisitos .
-                   "', t.informacoes = '" . $this->info . "', t.preco = " . $this->preco . " WHERE"
-                    . " cdTipoExame = " . $result["cdTipoExame"];
+            $sql = "UPDATE TB_TipoExame t SET t.coletaDomicilio = '" . $this->coletadomicilio
+                    . "', t.requisitos = '" . $this->requisitos .
+                    "', t.informacoes = '" . $this->info . "', t.preco = " . $this->preco 
+                    . " WHERE cdTipoExame = " . $result["cdTipoExame"];
         }
         else{
-            $sql = "INSERT INTO TB_TipoExame(cdTipoExame,nmTipoExame,coletaDomicilio,requisitos,informacoes,preco)" . 
-                   " VALUES ('','" . $this->nome . "','" . $this->coletadomicilio . "','" . $this->requisitos . "','" . 
-                   $this->info . "','" . $this->preco . "')";
+            $sql = "INSERT INTO TB_TipoExame(cdTipoExame,nmTipoExame,coletaDomicilio,"
+                    . "requisitos,informacoes,preco)" 
+                    . " VALUES ('','" . $this->nome . "','" . $this->coletadomicilio 
+                    . "','" . $this->requisitos . "','" 
+                    . $this->info . "','" . $this->preco . "')";
         }
         
         //Executa SQL e testa sucesso
-        $result = mysql_query($sql,$con) or die('Não foi possível salvar TipoExame no banco de dados: '.mysql_error());
-        
-        mysql_close($con);
+        $this->query($sql) or die('Não foi possível salvar TipoExame'
+                . ' no banco de dados: '. $this->dberror());
+    }
+    public function remove() {
+        $sql = "UPDATE TB_TipoExame t SET t.coletaDomicilio = '" . $this->coletadomicilio
+                    . "', t.requisitos = '" . $this->requisitos .
+                    "', t.informacoes = '" . $this->info . "', t.preco = " . $this->preco 
+                    . " WHERE nmTipoExame = " . $this->nome;
+        $this->query($sql) or die('Não foi possível remover TipoExame'
+                . ' do banco de dados: '. $this->dberror());
     }
 }
 
