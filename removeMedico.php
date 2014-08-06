391846c269bfc92a1934229a973bba5006a60fc4
EliasLuiz <eliasluizjr@gmail.com>
2014-08-06 10:01:26 -0300

    Adicionado Remove Médico

--- removeMedico.php
+++ removeMedico.php
@@ -3,5 +3,13 @@
     // DELETAR?
 
     require_once 'html/cadastros/configuracoes/removemedicos.html';
+    require_once 'classes/M/Medico.php';
+    
+    if(isset($_POST['nomemedico'])){
+        $medico = new Medico();
+        $lista = $medico->lista();
+        $medico->carrega($lista[$_POST['nomemedico']]);
+        $medico->remove();
+    }
 
 ?>
