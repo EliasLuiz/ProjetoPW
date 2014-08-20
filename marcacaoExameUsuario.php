d32e50b607ec0fa2a16735d67b1b1f52d5e83f4d
EliasLuiz <eliasluizjr@gmail.com>
2014-08-20 10:35:11 -0300

    Arquivo renomeado

---
+++ marcacaoExameUsuario.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'classes/V/IMarcacaoExame.php';
+
+if (isset($_POST['cd'])) {
+    $interf = new IMarcacaoExame();
+    $interf->marcaExame($_SESSION['cd'], $_POST['cdMedico'], $_POST['cdConsulta'],
+            $_POST['cdTipoExame'], $_POST['cdConvenio'], $_POST['dataExame'], 
+            $_POST['dataColeta']);
+}
+?>
