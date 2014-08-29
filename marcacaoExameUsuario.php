63489616e9b9b7993ac4c4b5c509aac0fa465776
EliasLuiz <eliasluizjr@gmail.com>
2014-08-28 21:42:48 -0300

    Commit de debug - adicionado valores temporarios para alguns items post

--- marcacaoExameUsuario.php
+++ marcacaoExameUsuario.php
@@ -2,6 +2,10 @@
 session_start();
 require_once __DIR__ . '/classes/V/IMarcacaoExame.php';
 
+$_POST['cdMedico'] = "";
+$_POST['cdConsulta'] = "";
+$_POST['dataColeta'] = "";
+
 if (isset($_POST['exames1'])) {
     $interf = new IMarcacaoExame();
     $interf->marcaExame($_SESSION['cd'], $_POST['cdMedico'], $_POST['cdConsulta'],
