7186250d2f7ff1720aed43dcfef1106d530fd80c
daniel <daniel@daniel-RF511-RF411-RF711>
2014-08-28 21:37:38 -0300

    debug de envio de exame

--- marcacaoExameUsuario.php
+++ marcacaoExameUsuario.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require_once __DIR__ . '/classes/V/IMarcacaoExame.php';
 
 if (isset($_POST['exames1'])) {
--- marcacaoexame.php
+++ marcacaoexame.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+//session_start();
 require_once __DIR__ . '/classes/M/TipoExame.php';
 require_once __DIR__ . '/classes/M/Convenio.php';
 
