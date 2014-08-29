955ddcd60c208be25b710f6e243b33bceb6bd957
EliasLuiz <eliasluizjr@gmail.com>
2014-08-28 21:25:32 -0300

    Commit de debug - alterado cd para exames 1

--- marcacaoExameUsuario.php
+++ marcacaoExameUsuario.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/classes/V/IMarcacaoExame.php';
 
-if (isset($_POST['cd'])) {
+if (isset($_POST['exames1'])) {
     $interf = new IMarcacaoExame();
     $interf->marcaExame($_SESSION['cd'], $_POST['cdMedico'], $_POST['cdConsulta'],
             $_POST['exames1'], $_POST['convenio'], $_POST['data'], 
